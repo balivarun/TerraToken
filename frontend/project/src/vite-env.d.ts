c7b81d1b10850c0a40af25ddeae88e0efbe3824c
balivarun <varunbali345@gamil.com>
2025-04-23 18:32:23 +0530

    enhance the vite-env.d.ts file

--- frontend/project/src/vite-env.d.ts
+++ frontend/project/src/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  VITE_API_URL: string
+  VITE_APP_TITLE: string
+  // Add other custom env variables here
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
